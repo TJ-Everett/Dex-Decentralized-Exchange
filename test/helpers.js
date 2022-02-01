58c68343038b73d8081556fa0454297e30ae50e0
System Administrator <root@Taylers-MacBook-Pro.local>
2022-02-01 13:27:34 -0700

    make it better

---
+++ test/helpers.js
@@ -0,0 +1,18 @@
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const EVM_REVERT = 'VM Exception while processing transaction: revert'
+
+export const ether = (n) => {
+	return new web3.utils.BN(
+	web3.utils.toWei(n.toString(), 'ether')
+)
+}
+
+export const tokens = (n) => ether(n)
+
+export const formatBalance = (balance) => {
+	const precision = 100 // 2 decimal places 
+	balance = ether(balance)
+	balance = Math.round(balance * precision) / precision // Use 2 decimal places 
+	return balance
+}
